2f38c1fd81180f50e928288734e03cc0c03c425d
jbharwood <josephblakeharwood@gmail.com>
2019-01-25 11:24:01 -0500

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,10 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :show
+  belongs_to :actor
+
+  def say_that_thing_you_say
+    urkel = Character.find_by(name: "Steve Urkel")
+    "Steve Urkel always says: #{urkel.catchphrase}"
+  end
+
 end
--- db/migrate/008_add_character_catchphrase_column.rb
+++ db/migrate/008_add_character_catchphrase_column.rb
@@ -1,11 +1,8 @@
 # DO NOT EDIT!
 # If you need to add things to shows, you need to create a new migration file
 # and additively change the schema. Always add new migrations to change the db.
-class CreateShows < ActiveRecord::Migration[4.2]
+class AddCharacterCatchphraseColumn < ActiveRecord::Migration[4.2]
   def change
-    create_table :shows do |t|
-      #primary key of :id is created for us!
-      t.string :name
-    end
+    add_column :characters, :catchphrase, :string
   end
 end
